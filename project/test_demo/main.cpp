6d52853e23a17f034a3c04713fb6d219a4770127
gerald0057 <1565179402@qq.com>
2022-12-27 19:45:23 +0800

    update code for tool button

--- project/test_demo/main.cpp
+++ project/test_demo/main.cpp
@@ -1,13 +1,39 @@
-#include "widget.h"
+//#include "widget.h"
 
 #include <QApplication>
+
+#include <QToolButton>
+#include <QMenu>
+#include <QIcon>
+#include <QSize>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    Widget w;
-    w.show();
+//    Widget w;
+//    w.show();
+
+    QToolButton btn;
+    QMenu menu;
+
+    menu.addAction(QString("Setting"));
+    menu.addAction(QString("Version"));
+    menu.addAction(QString("About"));
+    menu.addAction(QString("Exit"));
+
+    btn.setText("Menu");
+    btn.setMenu(&menu);
+    btn.setPopupMode(QToolButton::MenuButtonPopup);
+
+//    btn.setArrowType(Qt::DownArrow);
+//    btn.setToolButtonStyle(Qt::ToolButtonIconOnly);
+
+    btn.setIcon(QIcon("icon.png"));
+    btn.setIconSize(QSize(40, 40));
+    btn.setToolButtonStyle(Qt::ToolButtonTextUnderIcon);
+
+    btn.show();
 
     return a.exec();
 }
