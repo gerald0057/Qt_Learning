ded0f5d571c2e7c6f6b3bf54564556d0fe1abc8a
gerald0057 <1565179402@qq.com>
2023-01-03 20:38:52 +0800

    update code

--- project/test_demo/main.cpp
+++ project/test_demo/main.cpp
@@ -1,4 +1,6 @@
-#include "widget.h"
+//#include "widget.h"
+#include <QProgressBar>
+#include <QHBoxLayout>
 
 #include <QApplication>
 
@@ -9,12 +11,29 @@ int main(int argc, char *argv[])
 //    Widget w;
 //    w.show();
 
-    customSpinBox box;
-    box.setRange(2, 10);
-    box.setSingleStep(2);
-    box.setValue(4);
-    box.setWrapping(true);
-    box.show();
+//    customSpinBox box;
+//    box.setRange(2, 10);
+//    box.setSingleStep(2);
+//    box.setValue(4);
+//    box.setWrapping(true);
+//    box.show();
+
+    QWidget w;
+
+    QProgressBar bar(&w);
+    bar.setMaximum(100);
+    bar.setMinimum(0);
+    bar.setValue(50);
+    bar.setFormat(QString("download :%1%").arg(QString::number(bar.value() * 100 / bar.maximum(), 'f', 1)));
+    bar.setStyleSheet("text-align:right");
+
+    QHBoxLayout layout(&w);
+    layout.addWidget(&bar);
+
+//    QWidget::connect(&box, SIGNAL(valueChanged(int)), &slider, SLOT(setValue(int)));
+//    QWidget::connect(&slider, SIGNAL(valueChanged(int)), &box, SLOT(setValue(int)));
+
+    w.show();
 
     return a.exec();
 }
