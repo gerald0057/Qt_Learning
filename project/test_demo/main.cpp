ca2cfe623f9b01f803934f16d58a44014cb9a43f
gerald0057 <1565179402@qq.com>
2023-01-05 21:25:53 +0800

    update project code

--- project/test_demo/main.cpp
+++ project/test_demo/main.cpp
@@ -1,12 +1,27 @@
-#include "widget.h"
+//#include "widget.h"
 #include <QApplication>
+#include <QTableWidget>
+#include <QHeaderView>
+#include <QVBoxLayout>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    Widget w;
+//    Widget w;
+//    w.show();
+    QWidget w;
+
+    QTableWidget tab(3, 4, &w);
+
+    tab.horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    tab.setContextMenuPolicy(Qt::CustomContextMenu);
+
+    QVBoxLayout layout(&w);
+    layout.addWidget(&tab);
+
     w.show();
+
 
     return a.exec();
 }
